f57140840b4d3d41575d7f858081662e52b66e4a
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-02-20 21:55:12 -0800

    [ADDED] Components & Pages

---
+++ src/components/Meal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Meal = () => {
+  return (
+    <div>
+      <h2>Meal information</h2>
+    </div>
+  );
+}
+
+export default Meal;
---
+++ src/components/MealList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Meal from './Meal'
+import Loading from './Loading'
+// import { useGlobalContext } from '../context'
+
+const MealList = () => {
+  return (
+    <div>
+      <h2>Meal list information</h2>
+    </div>
+  );
+}
+
+export default MealList;
